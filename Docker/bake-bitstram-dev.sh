f68787d1d9560f2e20dacafdba179f68707211be
Lovis König <lovis@outlook.com>
2024-02-04 17:23:33 +0100

    correct broken file

--- Docker/bake-bitstram-dev.sh
+++ Docker/bake-bitstram-dev.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-if [$1 -eq "git"];
-    docker buildx bake build-bitstream-dev $2
-else
-    docker buildx bake bitstream-dev --set bitstream-dev.tags=lvskng/main:gbdotlive-bitstream-dev $1;
-fi
+docker buildx bake bitstream-dev --set bitstream-dev.tags=lvskng/main:gbdotlive-bitstream-dev $1;
 docker push lvskng/main:gbdotlive-bitstream-dev
